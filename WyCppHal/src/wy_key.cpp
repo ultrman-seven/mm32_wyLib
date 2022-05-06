905fc7913a1a39a9bab6209b0ff335facd4421d0
Leslie <1813180652@qq.com>
2022-05-07 02:29:02 +0800

    更新

--- WyCppHal/src/wy_key.cpp
+++ WyCppHal/src/wy_key.cpp
@@ -10,38 +10,74 @@ namespace KEY
 
     public:
         KEY_Object() = default;
-        KEY_Object();
+        KEY_Object(GPIO_TypeDef *port, uint16_t pin) { KEY_Object(port, pin, true, 0); };
+        KEY_Object(GPIO_TypeDef *port, uint16_t pin, bool interrupt, uint8_t p);
     };
 
 } // namespace KEY
 
 using namespace KEY;
 
-// >>> for i in range(16):
-// ...     print(f'void (*e{i}Callback)(void)=nullptr;')
-// >>>
-void (*e0Callback)(void) = nullptr;
-void (*e1Callback)(void) = nullptr;
-void (*e2Callback)(void) = nullptr;
-void (*e3Callback)(void) = nullptr;
-void (*e4Callback)(void) = nullptr;
-void (*e5Callback)(void) = nullptr;
-void (*e6Callback)(void) = nullptr;
-void (*e7Callback)(void) = nullptr;
-void (*e8Callback)(void) = nullptr;
-void (*e9Callback)(void) = nullptr;
-void (*e10Callback)(void) = nullptr;
-void (*e11Callback)(void) = nullptr;
-void (*e12Callback)(void) = nullptr;
-void (*e13Callback)(void) = nullptr;
-void (*e14Callback)(void) = nullptr;
-void (*e15Callback)(void) = nullptr;
+void (*extiCallback[16])(void) = {nullptr};
+
+KEY_Object::KEY_Object(GPIO_TypeDef *port, uint16_t pin, bool interrupt, uint8_t p)
+{
+    this->port = port;
+    this->pin = pin;
+    uint8_t p_s = pin2pinSource(pin);
+
+    if (interrupt)
+    {
+        RCC_APB2PeriphClockCmd(RCC_APB2Periph_SYSCFG, ENABLE);
+        uint16_t gpio_s = (uint32_t)(this->port) & 0xffff;
+        SYSCFG_EXTILineConfig(gpio_s / 0x400, p_s);
+
+        NVIC_InitTypeDef nvic;
+        EXTI_InitTypeDef exti;
+        switch (p_s)
+        {
+        case 0:
+        case 1:
+            nvic.NVIC_IRQChannel = EXTI0_1_IRQn;
+            break;
+        case 2:
+        case 3:
+            nvic.NVIC_IRQChannel = EXTI2_3_IRQn;
+            break;
+        case 4:
+        case 5:
+        case 6:
+        case 7:
+        case 8:
+        case 9:
+        case 10:
+        case 11:
+        case 12:
+        case 13:
+        case 14:
+        case 15:
+            nvic.NVIC_IRQChannel = EXTI4_15_IRQn;
+            break;
+
+        default:
+            break;
+        }
+        nvic.NVIC_IRQChannelCmd = ENABLE;
+        nvic.NVIC_IRQChannelPriority = p;
+        NVIC_Init(&nvic);
+        exti.EXTI_Line = this->pin;
+        exti.EXTI_LineCmd = ENABLE;
+        exti.EXTI_Mode = EXTI_Mode_Interrupt;
+        exti.EXTI_Trigger = EXTI_Trigger_Falling;
+        EXTI_Init(&exti);
+    }
+}
 
 //>>> def exti_i(i:int):
 //...     r=f'''if (EXTI_GetITStatus(EXTI_Line{i}) != RESET)
 //... {{
-//...     if (e{i}Callback != nullptr)
-//...         e{i}Callback();
+//...     if (extiCallback[{i}] != nullptr)
+//...         extiCallback[{i}]();
 //...     EXTI_ClearITPendingBit(EXTI_Line{i});
 //... }}'''
 //...     print(r)
@@ -55,14 +91,14 @@ void EXTI0_1_IRQHandler(void)
 {
     if (EXTI_GetITStatus(EXTI_Line0) != RESET)
     {
-        if (e0Callback != nullptr)
-            e0Callback();
+        if (extiCallback[0] != nullptr)
+            extiCallback[0]();
         EXTI_ClearITPendingBit(EXTI_Line0);
     }
     if (EXTI_GetITStatus(EXTI_Line1) != RESET)
     {
-        if (e1Callback != nullptr)
-            e1Callback();
+        if (extiCallback[1] != nullptr)
+            extiCallback[1]();
         EXTI_ClearITPendingBit(EXTI_Line1);
     }
 }
@@ -71,89 +107,89 @@ void EXTI2_3_IRQHandler(void)
 {
     if (EXTI_GetITStatus(EXTI_Line2) != RESET)
     {
-        if (e2Callback != nullptr)
-            e2Callback();
+        if (extiCallback[2] != nullptr)
+            extiCallback[2]();
         EXTI_ClearITPendingBit(EXTI_Line2);
     }
     if (EXTI_GetITStatus(EXTI_Line3) != RESET)
     {
-        if (e3Callback != nullptr)
-            e3Callback();
+        if (extiCallback[3] != nullptr)
+            extiCallback[3]();
         EXTI_ClearITPendingBit(EXTI_Line3);
     }
 }
+
 void EXTI4_15_IRQHandler(void)
 {
     if (EXTI_GetITStatus(EXTI_Line4) != RESET)
     {
-        if (e4Callback != nullptr)
-            e4Callback();
+        if (extiCallback[4] != nullptr)
+            extiCallback[4]();
         EXTI_ClearITPendingBit(EXTI_Line4);
     }
     if (EXTI_GetITStatus(EXTI_Line5) != RESET)
     {
-        if (e5Callback != nullptr)
-            e5Callback();
+        if (extiCallback[5] != nullptr)
+            extiCallback[5]();
         EXTI_ClearITPendingBit(EXTI_Line5);
     }
     if (EXTI_GetITStatus(EXTI_Line6) != RESET)
     {
-        if (e6Callback != nullptr)
-            e6Callback();
+        if (extiCallback[6] != nullptr)
+            extiCallback[6]();
         EXTI_ClearITPendingBit(EXTI_Line6);
     }
     if (EXTI_GetITStatus(EXTI_Line7) != RESET)
     {
-        if (e7Callback != nullptr)
-            e7Callback();
+        if (extiCallback[7] != nullptr)
+            extiCallback[7]();
         EXTI_ClearITPendingBit(EXTI_Line7);
     }
     if (EXTI_GetITStatus(EXTI_Line8) != RESET)
     {
-        if (e8Callback != nullptr)
-            e8Callback();
+        if (extiCallback[8] != nullptr)
+            extiCallback[8]();
         EXTI_ClearITPendingBit(EXTI_Line8);
     }
     if (EXTI_GetITStatus(EXTI_Line9) != RESET)
     {
-        if (e9Callback != nullptr)
-            e9Callback();
+        if (extiCallback[9] != nullptr)
+            extiCallback[9]();
         EXTI_ClearITPendingBit(EXTI_Line9);
     }
     if (EXTI_GetITStatus(EXTI_Line10) != RESET)
     {
-        if (e10Callback != nullptr)
-            e10Callback();
+        if (extiCallback[10] != nullptr)
+            extiCallback[10]();
         EXTI_ClearITPendingBit(EXTI_Line10);
     }
     if (EXTI_GetITStatus(EXTI_Line11) != RESET)
     {
-        if (e11Callback != nullptr)
-            e11Callback();
+        if (extiCallback[11] != nullptr)
+            extiCallback[11]();
         EXTI_ClearITPendingBit(EXTI_Line11);
     }
     if (EXTI_GetITStatus(EXTI_Line12) != RESET)
     {
-        if (e12Callback != nullptr)
-            e12Callback();
+        if (extiCallback[12] != nullptr)
+            extiCallback[12]();
         EXTI_ClearITPendingBit(EXTI_Line12);
     }
     if (EXTI_GetITStatus(EXTI_Line13) != RESET)
     {
-        if (e13Callback != nullptr)
-            e13Callback();
+        if (extiCallback[13] != nullptr)
+            extiCallback[13]();
         EXTI_ClearITPendingBit(EXTI_Line13);
     }
     if (EXTI_GetITStatus(EXTI_Line14) != RESET)
     {
-        if (e14Callback != nullptr)
-            e14Callback();
+        if (extiCallback[14] != nullptr)
+            extiCallback[14]();
         EXTI_ClearITPendingBit(EXTI_Line14);
     }
     if (EXTI_GetITStatus(EXTI_Line15) != RESET)
     {
-        if (e15Callback != nullptr)
-            e15Callback();
+        if (extiCallback[15] != nullptr)
+            extiCallback[15]();
         EXTI_ClearITPendingBit(EXTI_Line15);
     }
-}
